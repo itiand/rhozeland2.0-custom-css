27081b23f160d133dc3ab798dbcbc62301d4831e
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-01-25 17:51:31 -0500

    implement dynamic tabview

--- tab.js
+++ tab.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll("[data-tab-target]");
-  console.log("tabs", tabs);
+  const tabViews = document.querySelectorAll(".tab-view");
 
   tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
       const selectedTabView = document.querySelector(tab.dataset.tabTarget);
-      selectedTabView.classList.add("red-border");
+
+      //remove active class to all
+      tabViews.forEach((tabView) => {
+        tabView.classList.remove("active-tab");
+      });
+
+      //add active class to that one target
+      selectedTabView.classList.add("active-tab");
       console.log("RED BORDER ADDED ON", selectedTabView);
     });
   });
