27dc0d63bc79d9e5b26e1f1ead5ccf77029a64e3
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-01-29 12:44:07 -0500

    implement dynamic tab-target change

--- tab.js
+++ tab.js
@@ -1,21 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const tabs = document.querySelectorAll("[data-tab-target]");
+  const tabs = document.querySelectorAll("[data-tab-target]"); // each tabItem
   const tabViews = document.querySelectorAll(".tab-view");
-
-  //
 
   tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
       const selectedTabView = document.querySelector(tab.dataset.tabTarget);
 
-      //remove active class to all
+      //remove active class to all tab-views
       tabViews.forEach((tabView) => {
         tabView.classList.remove("active-tab");
       });
 
+      //remove active class to all tab-items
+      tabs.forEach((tab) => {
+        tab.classList.remove("active-tab");
+      });
+
       //add active class to that one target
+      ////tab-view
       selectedTabView.classList.add("active-tab");
-      console.log("RED BORDER ADDED ON", selectedTabView);
+      ////tab-items__i
+      tab.classList.add("active-tab");
     });
   });
 });
