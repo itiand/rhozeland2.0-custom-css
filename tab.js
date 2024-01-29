cd6fc3f8d86ba75d229bc059d846920d503c7e29
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-01-29 17:50:40 -0500

    scale dynamic tab view to more sections

--- tab.js
+++ tab.js
@@ -1,26 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll("[data-tab-target]"); // each tabItem
-  const tabViews = document.querySelectorAll(".tab-view");
+  const tabViews = document.querySelectorAll(".tab-view"); //everything with a tab-view class
 
   tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
-      const selectedTabView = document.querySelector(tab.dataset.tabTarget);
+      const selectedTabView = document.querySelectorAll(tab.dataset.tabTarget);
 
-      //remove active class to all tab-views
-      tabViews.forEach((tabView) => {
-        tabView.classList.remove("active-tab");
-      });
-
-      //remove active class to all tab-items
+      //remove active class to all tab-headings
       tabs.forEach((tab) => {
         tab.classList.remove("active-tab");
       });
 
+      //remove active class to all elements with tab-views, --> therefore hides
+      tabViews.forEach((tabView) => {
+        tabView.classList.remove("active-tab");
+      });
+
       //add active class to that one target
       ////tab-view
-      selectedTabView.classList.add("active-tab");
+      selectedTabView.forEach((view) => {
+        view.classList.add("active-tab");
+      });
       ////tab-items__i
       tab.classList.add("active-tab");
+      ////
     });
   });
 });
